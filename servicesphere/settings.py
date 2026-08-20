4748fd06e65c6e6d9f3b648204cf2b9f3ead09fc
Tanyy12 <tanyasinha712@gmail.com>
2026-08-20 23:58:48 +0530

    SQLite to MySQL done

--- servicesphere/settings.py
+++ servicesphere/settings.py
@@ -123,8 +123,17 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME' : config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD' : config('DB_PASSWORD'),
+        'HOST' : config('DB_HOST'),
+        'PORT' : config('DB_PORT'),
+        'OPTIONS' : {
+            'charset' : 'utf8mb4',
+        },
     }
 }
 
